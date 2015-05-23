5d379d2677b9ee94719a683b848229857bef62e5
Brice Arnould <unbrice@vleu.net>
2015-05-24 00:34:58 +0200

    Update sphinx to 1.3
    Version 1.3 bundles Napoleon, and requires RTD_theme >= 0.18

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -19,9 +19,9 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
 ]
 
