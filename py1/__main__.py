54b3d3c3c19330dfcb3459b270c2959021c29e9c
Brice Arnould <unbrice@vleu.net>
2015-05-24 00:19:39 +0200

    Update copyright

--- py1/__main__.py
+++ py1/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2013, Brice Arnould <unbrice@vleu.net>
+# Copyright (c) 2013-2015, Brice Arnould <unbrice@vleu.net>
 # All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following condition are met:
--- py1/template_reader.py
+++ py1/template_reader.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013, Brice Arnould <unbrice@vleu.net>
+# Copyright (c) 2013-2015, Brice Arnould <unbrice@vleu.net>
 # All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following condition are met:
