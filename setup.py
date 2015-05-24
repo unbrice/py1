80ccc4d7d82bdc109b59a347c9f6ff35e48c5d4b
Brice Arnould <unbrice@vleu.net>
2015-05-24 18:40:35 +0200

    Remove support of Python 3.1&3.2 from setup.py

--- setup.py
+++ setup.py
@@ -97,8 +97,6 @@ setuptools.setup(
         'Environment :: Console',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
