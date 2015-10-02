cfeaf49d41eeb76dd9a5a29c3a2c1b0dc3caf9e8
Brice Arnould <unbrice@vleu.net>
2015-10-02 13:48:04 -0400

    Declares support of Python 3.5

--- setup.py
+++ setup.py
@@ -111,6 +111,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 
     packages=['py1'],
